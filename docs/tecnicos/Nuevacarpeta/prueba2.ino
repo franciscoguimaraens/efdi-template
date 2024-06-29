9685ef2730632892ed70ec954c308829cf0ac9e0
franciscoguimaraens <guimaraensfrancisco@gmail.com>
2024-06-28 23:26:13 -0300

    ssi

---
+++ docs/tecnicos/Nuevacarpeta/prueba2.ino
@@ -0,0 +1,47 @@
+#include <Servo.h>
+
+Servo servo;
+
+int inches = 0;
+
+int cm = 0;
+
+int servopos;
+
+long readUltrasonicDistance(int triggerPin, int echoPin)
+{
+  pinMode(triggerPin, OUTPUT);  // Clear the trigger
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigger pin to HIGH state for 10 microseconds
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(triggerPin, LOW);
+  pinMode(echoPin, INPUT);
+  // Reads the echo pin, and returns the sound wave travel time in microseconds
+  return pulseIn(echoPin, HIGH);
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  servo.attach(4);
+}
+
+void loop()
+{
+  // measure the ping time in cm
+  cm = 0.01723 * readUltrasonicDistance(7, 7);
+  // convert to inches by dividing by 2.54
+  inches = (cm / 2.54);
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(cm);
+  Serial.println("cm");
+  servopos = constrain(servopos, 0 , 180);
+  servopos = map(cm, 2, 300, 0, 180);
+  Serial.print(servopos);
+  Serial.println("posicion");
+  servo.write (servopos);
+  delay(100); // Wait for 100 millisecond(s)
+}
